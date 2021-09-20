7b081cae552e9ce46b24565abb5acd85168fbba1
LazyKeru <killian.allaire@hotmail.com>
2021-09-20 16:36:33 +0200

    chore: added the required dependencies for Serving Web Content with spring mvc

---
+++ src/main/java/com/lazykeru/tpwebjavaimt/TpwebjavaimtApplication.java
@@ -0,0 +1,13 @@
+package com.lazykeru.tpwebjavaimt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpwebjavaimtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpwebjavaimtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazykeru/tpwebjavaimt/TpwebjavaimtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazykeru.tpwebjavaimt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpwebjavaimtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
